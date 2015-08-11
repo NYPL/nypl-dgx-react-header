71f8a3e659bf72522a9cfe255af7823e4d969375
Rafael Hernandez <rafaelhernandez@nypl.org>
2015-08-11 17:11:35 -0400

    Updated application's config file apiUrl to include children and related-mega-menu-items

--- appConfig.js
+++ appConfig.js
@@ -1,7 +1,7 @@
 const config = {
 	appName: 'NYPL React Header',
 	port: 3001,
-	apiUrl: 'http://dev.refinery.aws.nypl.org/api/nypl/ndo/v0.1/site-data/header-items'
+	apiUrl: 'http://dev.refinery.aws.nypl.org/api/nypl/ndo/v0.1/site-data/header-items?include=children,related-mega-menu-panes.current-mega-menu-items.images'
 };
 
 export default config;
