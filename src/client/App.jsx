d7ce6603a53d34a3b324d17716aff49d26c77a9f
Rafael Hernandez <rafaelhernandez@nypl.org>
2015-08-13 16:06:15 -0400

    Added conditional check for server side versus client side rendering on App.jsx which loads the app.

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -6,10 +6,22 @@ import Header from '../app/components/Header/Header.jsx';
 import './styles/main.scss';
 
 if (typeof window !== 'undefined') {
+
+	let isRenderedByServer = false;
+
 	window.onload = () => {
+		// Render Isomorphically
 	  Iso.bootstrap(function (state, meta, container) {
+	  	console.log('Application rendered Isomorphically.');
 	    alt.bootstrap(state);
 	    React.render(React.createElement(Header), container);
+	    isRenderedByServer = true;
 	  });
+
+	  // Render Client Side Only, attached to ID
+	  if (!isRenderedByServer) {
+	  	console.log('Application rendered via Client');
+	  	React.render(React.createElement(Header), document.getElementById('nypl-dgx-header'));
+	  }
 	}
 }
