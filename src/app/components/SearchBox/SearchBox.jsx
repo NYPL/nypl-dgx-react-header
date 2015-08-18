87e4dda08c4083a2eaa18108d7ff97cece15640e
Kang Peng <kangpeng@MA-DIGXP.local>
2015-08-18 17:54:35 -0400

    Added texts and link in search box

--- src/app/components/SearchBox/SearchBox.jsx
+++ src/app/components/SearchBox/SearchBox.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 import Radium from 'radium';
 import cx from 'classnames';
 
+// Import components
+import SimpleButton from '../Buttons/SimpleButton.jsx';
+
 class SearchBox extends React.Component {
   // Constructor used in ES6
   constructor(props) {
@@ -17,14 +20,14 @@ class SearchBox extends React.Component {
       <div id='NavMenu__Search-Box' className={'NavMenu__Search-Box'+classes} 
       style={this.props.isActive ? styles.show : styles.hide}>
         <form action='' method='GET'>
-          <input type='text' id='NavMenu__Search-Box__Input-Field' value='What do you want to search?' />
+          <input type='text' id='NavMenu__Search-Box__Input-Field' value='What would you like to find?' />
           <input type='submit' value='submit' id='NavMenu__Search-Box__Submit-Btn' />
           <br />
-          <input type='radio' name='sex' value='male' checked />Male
+          <input type='radio' name='sex' value='catalog' checked />Search the Catalog
           <br />
-          <input type='radio' name='sex' value='female' />Female
+          <input type='radio' name='sex' value='org' />Search NYPL.org
         </form>
-        <a href='http://catalog.nypl.org/'>Advenced Search</a>
+        <SimpleButton target='http://catalog.nypl.org/' label='Advenced Search'/>
       </div>
     );
   }
