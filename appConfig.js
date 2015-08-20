5d802ff44b8d627c709b1255ce5cf60b0e46fb63
Rafael Hernandez <rafaelhernandez@nypl.org>
2015-08-20 16:47:41 -0400

    Updated NPM commands, cleaned up serverConfig to use ES6 function declarations. Added colors module.

--- appConfig.js
+++ appConfig.js
@@ -1,6 +1,8 @@
 const config = {
-	appName: 'NYPL | React Header',
+	appTitle: 'NYPL | React Header',
+	appName: 'NYPL DGX React Header',
 	port: 3001,
+	webpackPort: 3000,
 	favIconPath: 'http://ux-static.nypl.org.s3-website-us-east-1.amazonaws.com/images/favicon.ico',
 	apiUrl: 'http://dev.refinery.aws.nypl.org/api/nypl/ndo/v0.1/site-data/header-items?filter%5Brelationships%5D%5Bparent%5D=null&include=children,related-mega-menu-panes.current-mega-menu-items.images'
 };
