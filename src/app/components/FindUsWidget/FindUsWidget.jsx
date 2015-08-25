88176d99340b2502353ec87d84f0f102b0004edd
Rafael Hernandez <rafaelhernandez@nypl.org>
2015-08-25 13:02:14 -0400

    WEB-772 Updated component with new validation methods. Updated all function blocks with proper comment blocks guided by AIRBNB's JS ES6 Styleguide. Updated handleChange() method to use this.refs instead of event.target.value. Seemed to be better based of FB react forms page.

--- src/app/components/FindUsWidget/FindUsWidget.jsx
+++ src/app/components/FindUsWidget/FindUsWidget.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-
 // Add-on Modules to generate FindUs Widget
 import LocationFinder from './LocationFinder.jsx';
 import FeaturedLocation from './FeaturedLocation.jsx';
-
 
 class FindUsWidget extends React.Component {
 
@@ -12,6 +10,19 @@ class FindUsWidget extends React.Component {
   }
   
   render() {
+    // This is pending removal once we establish the Data from API.
+    let mockedFeaturedLocation = {
+      title: 'George Bruce Library Celebrates 100 Years',
+      tag: 'Mocked Location Spotlight',
+      image: 'http://fpoimg.com/88x88',
+      desc: 'The original George Bruce Library was located on 42nd Street.'
+    };
+
+    // For now, we will utilize the 2nd item in the response
+    // since the first item is the LocationFinder Widget.
+    // If that response is empty, use the mocked data.
+    let featLocData = this.props.features[1] || mockedFeaturedLocation;
+
     return (
       <div className={this.props.className}>
         <LocationFinder 
@@ -19,10 +30,10 @@ class FindUsWidget extends React.Component {
         <div className={this.props.className + '-SeparationBar'}></div>
         <FeaturedLocation 
         className={this.props.className + '-FeaturedLocation'}
-        tag={'Location Spotlight'}
-        title={'George Bruce Library Celebrates 100 Years'}
-        image={'http://fpoimg.com/88x88'}
-        desc={'The original George Bruce Library was located on 42nd Street.'} />
+        tag={featLocData.tag}
+        title={featLocData.title}
+        image={featLocData.image}
+        desc={featLocData.desc} />
       </div>
     );
   }
--- src/app/components/FindUsWidget/LocationFinder.jsx
+++ src/app/components/FindUsWidget/LocationFinder.jsx
@@ -5,10 +5,11 @@ class LocationFinder extends React.Component {
   constructor(props) {
     super(props);
 
-    // Holds the initial state
-    // replaces getInitialState() method
+    // Holds the initial component state
     this.state = {
-    	zipcode: null
+    	zipcode: null,
+      disableSubmit: true,
+      nearestLocation: null
     };
 
     this._handleSubmit = this._handleSubmit.bind(this);
@@ -16,7 +17,9 @@ class LocationFinder extends React.Component {
   }
 
   render() {
+    // Reference the zipcode from the state.
   	let zipcode = this.state.zipcode;
+
     return (
       <div className={this.props.className}>
         <div className={this.props.className + '-Wrapper'}>
@@ -25,37 +28,107 @@ class LocationFinder extends React.Component {
             Type a zip code where you would like to find a NYPL Location.
           </label>
           <div className={this.props.className + '-SearchBox'}>
-            <input type='text' placeholder='Zip Code' id='LocationFinder-Input' ref='zipCodeInput' value={zipcode} onChange={this._handleChange} />
-            <button type='button' id='LocationFinder-Submit' onClick={this._handleSubmit}>FIND</button>
+            <input type='text' placeholder='Zip Code' id='LocationFinder-Input' ref='zipCodeInput' onChange={this._handleChange} />
+            <button type='button' id='LocationFinder-Submit' disabled={this.state.disableSubmit} onClick={this._handleSubmit}>FIND</button>
           </div>
         </div>
       </div>
     );
   }
 
+  /**
+   * _handleSubmit() 
+   * updates the state reference 
+   * which holds the nearestLocation
+   * property
+   *
+   * @return {Object} location
+   */
   _handleSubmit() {
 
   }
 
+  /**
+   * _enableSubmitButton() 
+   * sets the state's disableSubmit
+   * property value to false
+   *
+   */
+  _enableSubmitButton() {
+    this.setState({disableSubmit: false});
+  }
+
+  /**
+   * _getNearestLocations(zipcode) 
+   * returns the nearest location object
+   * from the list of NYPL locations
+   * based of an API.
+   *
+   * @param {Number} zipcode
+   * @return {Object} location
+   */
   _getNearestLocations(zipcode) {
 
   }
 
+  /**
+   * _validateZipcode(zipcode) 
+   * returns a boolean value based 
+   * on the validity of the zipcode param.
+   *
+   * @param {Number} zipcode
+   * @return {Boolean} true/false
+   */
   _validateZipcode(zipcode) {
   	if (zipcode && !isNaN(zipcode) && zipcode.length === 5) {
-  		console.log(zipcode);
   		return true;
   	}
   	return false;
   }
 
-  _handleChange(event) {
-  	let zipcode = event.target.value;
-  	this.setState({zipcode: zipcode});
+  /**
+   * _getZipcodeValue() 
+   * returns the current value
+   * from the state zipcode property
+   *
+   * @return {Number} state.zipcode
+   */
+  _getZipcodeValue() {
+   return this.state.zipcode;
+  }
+
+  /**
+   * _setZipcodeValue(value) 
+   * sets the state's zipcode
+   * property with the param value
+   *
+   * @param {Number} value
+   */
+  _setZipcodeValue(value) {
+    this.setState({zipcode: value});
+  }
+
+  /**
+   * _handleChange() 
+   * handles the changing value
+   * of the zipcode input and
+   * is responsible for firing off
+   * the nearest location suggestions
+   *
+   */
+  _handleChange() {
+    let zipcode = React.findDOMNode(this.refs.zipCodeInput).value;
 
   	if(this._validateZipcode(zipcode)) {
+
+      // Update the state.zipcode value
+      this._setZipcodeValue(zipcode);
+
+      // Enable the submitButton
+      this._enableSubmitButton();
+
   		// Execute Autocomplete Search Suggestions
-  		console.log('Input is valid, autocomplete should be enabled');
+  		console.log('Input is valid, suggestions should be enabled');
   	} else {
   		// Do not execute Autocomplete
   	}
