8d2339051959fc6997bb6d1b21e681139e1467bf
Edwin Guzman <edwin.gzmn@gmail.com>
2015-09-28 14:25:05 -0400

    Small comment update to the track event function.

--- src/app/utils/gaUtils.js
+++ src/app/utils/gaUtils.js
@@ -5,7 +5,8 @@ function gaUtils() {
    * _trackEvent(gaLabel)
    * Track a GA click event.
    *
-   * @param {gaLabel} String Label for GA event.
+   * @param {action} String Action for GA event.
+   * @param {label} String Label for GA event.
    */
   this._trackEvent = function _trackEvent(action, label) {
     ga.event({
