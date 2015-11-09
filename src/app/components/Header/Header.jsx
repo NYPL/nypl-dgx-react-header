42dfcbfddc99722f7295d2d7758ba52b81631dbc
Kang Peng <kang-tingpeng@MA-DIGXP-051149.local>
2015-11-09 18:37:06 -0500

    Got current window scroll Y position.

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -172,7 +172,7 @@ class Header extends React.Component {
    */
   _offsetStickyHeader() {
     if (window.location.hash) {
-      window.scrollBy(0, -(68+10));
+      window.scrollBy(0, this._getWindowVerticalScroll() + 10);
       console.log('scroll!!!!!!');
     }
   }
