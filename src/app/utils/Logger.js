14aaf964fe1e59233ee510a1ac7eea65dc11965d
Kang Peng <kangtingpeng@nypl.org>
2016-02-24 18:00:12 -0500

    modulralized transport settings.

--- src/app/utils/Logger.js
+++ src/app/utils/Logger.js
@@ -5,31 +5,25 @@ const nodeEnv = process.env.NODE_ENV;
 
 function Logger() {
   const logglyToken = process.env.LOGGLY_TOKEN;
-
-  // console.log(logglyToken);
+  const loggly = new Winston.transports.Loggly({
+    level: 'error',
+    handleExceptions: true,
+    inputToken: logglyToken,
+    subdomain: 'nypl',
+    tags: ['React-Header-App'],
+    json: false,
+    stripColors: true,
+  });
+  const console = new Winston.transports.Console({
+    level: 'debug',
+    handleExceptions: true,
+    json: false,
+    colorize: true,
+  });
 
   this.build = function() {
     return new Winston.Logger({
-      transports: (nodeEnv === 'production') ?
-      [
-        new Winston.transports.Loggly({
-          level: 'error',
-          handleExceptions: true,
-          inputToken: logglyToken,
-          subdomain: 'nypl',
-          tags: ['React-Header-App'],
-          json: false,
-          stripColors: true,
-        }),
-      ]
-      :[
-        new Winston.transports.Console({
-          level: 'debug',
-          handleExceptions: true,
-          json: false,
-          colorize: true,
-        }),
-      ],
+      transports: (nodeEnv === 'production') ? [loggly, console]:[console],
       exitOnError: false
     });
   }
