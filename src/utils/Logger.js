350e35352f88827aaf4d4d7402dc92c0e4725494
Kang Peng <kangtingpeng@nypl.org>
2016-02-25 17:39:01 -0500

    Updated the fallback for the missing of LOGGLY_TOKEN and LOGGLY_SUBDOMAIN.

--- src/utils/Logger.js
+++ src/utils/Logger.js
@@ -2,19 +2,21 @@ import Winston from 'winston';
 import 'winston-loggly';
 
 const nodeEnv = process.env.NODE_ENV;
+const logglyToken = process.env.LOGGLY_TOKEN || null;
+const logglyDomain = process.env.LOGGLY_SUBDOMAIN || null;
 
 function Logger() {
-  const logglyToken = process.env.LOGGLY_TOKEN || null;
-  const logglyDomain = process.env.LOGGLY_SUBDOMAIN || null;
-  const loggly = new Winston.transports.Loggly({
-    level: 'error',
-    handleExceptions: true,
-    inputToken: logglyToken,
-    subdomain: logglyDomain,
-    tags: ['Header-App'],
-    json: false,
-    stripColors: true,
-  });
+  const loggly = (logglyToken && logglyDomain) ?
+    new Winston.transports.Loggly({
+      level: 'error',
+      handleExceptions: true,
+      inputToken: logglyToken,
+      subdomain: logglyDomain,
+      tags: ['Header-App'],
+      json: false,
+      stripColors: true,
+    }
+  ): null;
   const console = new Winston.transports.Console({
     level: 'debug',
     handleExceptions: true,
@@ -24,7 +26,7 @@ function Logger() {
 
   this.build = function() {
     return new Winston.Logger({
-      transports: (nodeEnv !== 'development' && logglyToken && logglyDomain) ?
+      transports: (nodeEnv !== 'development' && loggly) ?
         [loggly, console]:[console],
       exitOnError: false
     });
