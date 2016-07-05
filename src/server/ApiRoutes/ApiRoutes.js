82156b9e827a50b708b291b9753b9bb65b4e0924
Edwin Guzman <edwin.gzmn@gmail.com>
2016-07-05 17:19:25 -0400

    WEB-3075 and WEB-3091 - Parsing header data, reading config file, matching feature items to header item uuids, and returning data based on url query that is read.

--- src/server/ApiRoutes/ApiRoutes.js
+++ src/server/ApiRoutes/ApiRoutes.js
@@ -8,6 +8,8 @@ import { refineryApi } from '../../../appConfig.js';
 // Logging
 import { getLogger } from 'dgx-loggly';
 
+import { findWhere as _findWhere } from 'underscore';
+
 // App environment settings
 const appEnvironment = process.env.APP_ENV || 'production';
 const apiRoot = refineryApi.root[appEnvironment];
@@ -31,6 +33,28 @@ const router = express.Router();
 // Assign full API url
 const completeApiUrl = parser.getCompleteApi(options);
 
+/* 
+ * getHeaderData()
+ * Parse the header endpoint response and add featured items to the selected IA array
+ * from the config, but based on the url query.
+ */
+const getHeaderData = (urlType, iaType, apiResponse) => {
+  const opts = { urlsAbsolute: (urlType === 'absolute') };
+  const iaArray = (iaType === 'upcoming') ? navConfig.upcoming : navConfig.current;
+  const parsed = parser.parse(apiResponse, options);
+  const modelData = Model.build(parsed, opts);
+
+  _.map(iaArray, headerItem => {
+    const item = _findWhere(modelData, { id: headerItem.id });
+
+    if (item) {
+      headerItem.features = item.features;
+    }
+  });
+
+  return iaArray;
+};
+
 /* Match the root or /isolated-header path
  * to populate the HeaderStore data from
  * the Header navConfig file's current IA.
@@ -38,47 +62,34 @@ const completeApiUrl = parser.getCompleteApi(options);
 router
   .route('/:var(header-markup)?')
   .get((req, res, next) => {
-    res.locals.data = {
-      HeaderStore: {
-        headerData: navConfig.current,
-        subscribeFormVisible: false,
-        myNyplVisible: false,
-      },
-      // Set the API URL here so we can access it when we
-      // render in the EJS file.
-      completeApiUrl,
-    };
-    next();
+    const urlType = req.query.urls || '';
+    const iaType = req.query.ia || '';
 
-    // const urlType = req.query.urls || '';
-    //
-    // axios
-    //   .get(completeApiUrl)
-    //   .then(data => {
-    //     const opts = { urlsAbsolute: (urlType === 'absolute') };
-    //     const parsed = parser.parse(data.data, options);
-    //     const modelData = Model.build(parsed, opts);
-    //
-    //     res.locals.data = {
-    //       HeaderStore: {
-    //         headerData: modelData,
-    //         subscribeFormVisible: false,
-    //         myNyplVisible: false
-    //       },
-    //       // Set the API URL here so we can access it when we
-    //       // render in the EJS file.
-    //       completeApiUrl
-    //     };
-    //     next();
-    //   })
-    //   .catch(error => {
-    //     logger.error(`Error calling API : ${completeApiUrl}. ${error}`);
-    //     // Set completeApiUrl for client side calling, if server side calling failed
-    //     res.locals.data = {
-    //       completeApiUrl
-    //     };
-    //     next();
-    //   });
+    axios
+      .get(completeApiUrl)
+      .then(data => {
+        const headerData = getHeaderData(urlType, iaType, data.data);
+
+        res.locals.data = {
+          HeaderStore: {
+            headerData,
+            subscribeFormVisible: false,
+            myNyplVisible: false
+          },
+          // Set the API URL here so we can access it when we
+          // render in the EJS file.
+          completeApiUrl
+        };
+        next();
+      })
+      .catch(error => {
+        logger.error(`Error calling API : ${completeApiUrl}. ${error}`);
+        // Set completeApiUrl for client side calling, if server side calling failed
+        res.locals.data = {
+          completeApiUrl
+        };
+        next();
+      });
       /* end Axios call */
   });
 
@@ -88,6 +99,7 @@ router
   .route('/header-data')
   .get((req, res) => {
     const urlType = req.query.urls || '';
+    const iaType = req.query.ia || '';
 
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -95,11 +107,9 @@ router
     axios
       .get(completeApiUrl)
       .then(data => {
-        const opts = { urlsAbsolute: (urlType === 'absolute') };
-        const parsed = parser.parse(data.data, options);
-        const modelData = Model.build(parsed, opts);
+        const headerData = getHeaderData(urlType, iaType, data.data);
 
-        res.json(modelData);
+        res.json(headerData);
       })
       .catch(error => {
         logger.log('Error calling API', error);
