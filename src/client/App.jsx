3a74d144e26e4d6c47c6b41c996b1dd5af842563
Edwin Guzman <edwin.gzmn@gmail.com>
2016-07-08 11:44:54 -0400

    Removing local config for fetching data.

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -17,7 +17,7 @@ import './styles/main.scss';
         alt.bootstrap(state);
         // Fire off the Feature Flag prior to render
         FeatureFlags.utils.activateFeature('shop-link');
-        ReactDOM.render(<Header env="local" />, container);
+        ReactDOM.render(<Header />, container);
         isRenderedByServer = true;
         console.log('nypl-dgx-header rendered isomorphically.');
       });
