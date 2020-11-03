3945ddf04bbc7a613e1b9a810e8b5a54e3994824
Gregory Kallenberg <gregorykallenberg@nypl.org>
2020-11-03 13:33:20 -0500

    adjustment to globalObject dom element

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ const commonSettings = {
     // Sets the name of the bundled application files
     // Additionally we can isolate vendor files as well
     filename: 'dgx-header.min.js',
-    globalObject: 'this',
+    globalObject: 'document',
   },
   plugins: [
     // Cleans the Dist folder after every build.
